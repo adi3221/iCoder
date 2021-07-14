4ee79191acba028975a07cd86842215c0395e4de
adi3221 <58384521+adi3221@users.noreply.github.com>
2021-07-15 01:38:17 +0530

    cool cmd

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5nptu--r-q+v1w^vofm-#wi&b(#_0l+rr(z-zfci%ficjtp4tv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
